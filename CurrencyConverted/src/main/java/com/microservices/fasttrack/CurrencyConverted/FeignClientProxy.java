670250122f8ca7385eecea4b91d3f15ce40d3b8a
Raghav <ragmohan108@gmail.com>
2022-05-19 17:06:40 +0800

    initial commit

---
+++ CurrencyConverted/src/main/java/com/microservices/fasttrack/CurrencyConverted/FeignClientProxy.java
@@ -0,0 +1,18 @@
+package com.microservices.fasttrack.CurrencyConverted;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+//@FeignClient(name="currency-conversion-factor", url = "localhost:8010")
+@FeignClient(name="currency-conversion-factor")
+public interface FeignClientProxy {
+
+	@GetMapping("/country/{code}")
+	public double getConversionFactor(
+			@PathVariable String code) ;
+	
+	
+}
